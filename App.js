e652c8a6569c2aa5b6ae91c282c2e18c863ca98e
Avinash Gholap <avinash.gholap@veritas.com>
2023-12-24 12:30:10 +0530

    Added react and react-dom as dependency

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('h1', {}, 'I am h1 tag'),
   React.createElement('h2', {}, 'I am h2 tag'),
